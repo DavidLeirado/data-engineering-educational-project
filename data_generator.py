94a7f41b03bf2af3a2c98f63c30b12cb0134f940
David Leirado Maroto <dleiradomaroto@gmail.com>
2023-08-22 10:19:58 +0200

    feat: basic random data generator skell

---
+++ data_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+class RandomDataGenerator:
+    def __init__(self):
+        """
+        Initial paramethers of the data random generator class
+        """
+        pass
+
+    def __iter__(self):
+        """
+        Sets up the class as an iterator class, returning itself
+        :return:
+        """
+        pass
+
+    def __next__(self):
+        """
+        Defines the logic to create new random data of corresponding type
+        :return:
+        """
+        pass
