0340e6efd61c14c0844092a3fc4046dcf75ebafc
David Leirado Maroto <dleiradomaroto@gmail.com>
2023-08-29 14:55:46 +0200

    feat: testing kafka script

---
+++ kafka_testing.py
@@ -0,0 +1,7 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer("probando", bootstrap_servers=['localhost:29092'], value_deserializer=lambda x: json.loads(x))
+
+for msg in consumer:
+    print(msg.value)
